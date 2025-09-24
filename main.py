fdb1cfe7bdfef82422dfa85f1a9d70cd03683972
Zazanicholas01 <matteoesposito12.me@gmail.com>
2025-09-24 15:26:56 +0200

    Model Trainer

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
+
+from networksecurity.entity.config_entity import (
+    DataIngestionConfig, 
+    TrainingPipelineConfig, 
+    DataValidationConfig, 
+    DataTransformationConfig, 
+    ModelTrainerConfig
+)
 
 import sys
 
@@ -11,6 +20,11 @@ if __name__ == '__main__':
 
     try:
         training_pipeline_config = TrainingPipelineConfig()
+
+        """
+        Data Ingestion
+        """
+
         data_ingestion_config = DataIngestionConfig(training_pipeline_config)
         data_ingestion = DataIngestion(data_ingestion_config)
  
@@ -19,6 +33,12 @@ if __name__ == '__main__':
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         print(data_ingestion_artifact)
 
+        logging.info("Data Ingestion completed")
+
+        """
+        Data Validation
+        """
+
         data_validation_config = DataValidationConfig(training_pipeline_config)
         data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
 
@@ -26,9 +46,13 @@ if __name__ == '__main__':
 
         data_validation_artifact = data_validation.initiate_data_validation()
 
-        logging.info("Data Validation Completed")
+        logging.info("Data Validation completed")
 
         print(data_validation_artifact)
+
+        """
+        Data Transformation
+        """
 
         data_transformation_config = DataTransformationConfig(training_pipeline_config)
         data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
@@ -37,9 +61,22 @@ if __name__ == '__main__':
 
         data_transformation_artifact = data_transformation.initiate_data_transformation()
 
-        logging.info("Data Transformation Completed")
+        logging.info("Data Transformation completed")
 
         print(data_transformation_artifact)
+
+        """
+        Model Trainer
+        """
+
+        model_trainer_config = ModelTrainerConfig(training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config, data_transformation_artifact)
+
+        logging.info("Initiate the Model Trainer")
+
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training completed")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -3,6 +3,16 @@ import sys
 import numpy as np
 import pandas as pd
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import (
+    AdaBoostClassifier,
+    GradientBoostingClassifier,
+    RandomForestClassifier
+)
+
 """
 Common constants for training pipeline
 """
@@ -18,6 +28,8 @@ TEST_FILE_NAME: str = "test.csv"
 SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
 PREPROCESSING_OBJECT_FILE_NAME: str = "preprocessor.pkl"
+
+SAVED_MODEL_DIR: str = os.path.join("saved_models")
 
 """
 Data Ingestion related constants
@@ -51,4 +63,50 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "missing_values": np.nan,
     "n_neighbors": 3,
     "weights": "uniform"
+}
+
+"""
+Model Trainer related constants
+"""
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD: float = 0.05
+
+models = {
+    "Random Forest": RandomForestClassifier(verbose=1),
+    "Decision Tree": DecisionTreeClassifier(),
+    "Gradient Boosting": GradientBoostingClassifier(verbose=1),
+    "Logistic Regression": LogisticRegression(verbose=1),
+    "AdaBoost": AdaBoostClassifier()
+}
+
+params={
+    "Decision Tree": {
+        'criterion':['gini', 'entropy', 'log_loss'],
+        # 'splitter':['best','random'],
+        # 'max_features':['sqrt','log2'],
+    },
+    "Random Forest":{
+        # 'criterion':['gini', 'entropy', 'log_loss'],
+        
+        # 'max_features':['sqrt','log2',None],
+        'n_estimators': [8,16,32,128,256]
+    },
+    "Gradient Boosting":{
+        # 'loss':['log_loss', 'exponential'],
+        'learning_rate':[.1,.01,.05,.001],
+        'subsample':[0.6,0.7,0.75,0.85,0.9],
+        # 'criterion':['squared_error', 'friedman_mse'],
+        # 'max_features':['auto','sqrt','log2'],
+        'n_estimators': [8,16,32,64,128,256]
+    },
+    "Logistic Regression":{},
+    "AdaBoost":{
+        'learning_rate':[.1,.01,.001],
+        'n_estimators': [8,16,32,64,128,256]
+    }
+    
 }
